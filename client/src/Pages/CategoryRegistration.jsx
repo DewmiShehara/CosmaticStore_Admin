39a5e37f9bf338db0e89152e40fa05ddd44017fb
Vimukthi Serasinghe <vimukthi.s@lnp.lk>
2025-07-28 22:43:13 +0530

    project client setup

---
+++ client/src/Pages/CategoryRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CategoryRegistration() {
+    return (
+        <div>
+            <h1>Category registration</h1>
+        </div>
+    )
+}
+
+export default CategoryRegistration
